d204eba757c0091135646b93fa08821495524455
daniel.geelen <daniel.geelen@c71387f0-5629-0410-9bdc-e9ce6186880e>
2007-02-25 21:31:35 +0000

    [misc] Some minor cleanup and adding of comments

--- src/gooey.cpp
+++ src/gooey.cpp
@@ -24,40 +24,37 @@ void tvMySharestarget_drag_data_received(GtkWidget          *widget,
                                         guint               info,
                                         guint               time){
   fprintf(stdout,"tvMySharestarget_drag_data_received:\nDATA=%s\n",data->data);
-  g_signal_stop_emission_by_name(widget,"drag_data_received");
-  }
+  /* TODO: 'Add URI to sharelist TreeView' (prob. requires making a ModelView for the TreeView)*/
+  g_signal_stop_emission_by_name(widget,"drag_data_received"); // Don't know if this is correct, but it makes GTK STFU
+}
 
 void btnSendMessage_clicked(GtkWidget *widget, gpointer user_data) {
   string s=gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSendMessage"));
   send_msg( s, port );
 }
 
-void btnReceive_clicked(GtkWidget *widget, gpointer user_data)
-{
+void btnReceive_clicked(GtkWidget *widget, gpointer user_data) {
   gtk_widget_set_sensitive(widget, false);
   string msg, from;
 
-  if( recv_msg(msg, port, &from) != SOCKET_ERROR) {
+  if( recv_msg(msg, port, &from) != SOCKET_ERROR) { //FIXME: What about other errors? UNIX way is better: '>= 0'
     msg = "received: '" + msg + "' from: " + from;
     gtk_entry_set_text((GtkEntry*) glade_xml_get_widget (main_window, "txtLastReceived"), msg.c_str());
   }
   gtk_widget_set_sensitive(widget, true);
 }
 
-void btnSetPort_clicked(GtkWidget *widget, gpointer user_data)
-{
+void btnSetPort_clicked(GtkWidget *widget, gpointer user_data) {
   int p = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSetPort")));
   UsePort(p);
 }
 
-void btnSetInterface_clicked(GtkWidget *widget, gpointer user_data)
-{
+void btnSetInterface_clicked(GtkWidget *widget, gpointer user_data) {
   int i = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtInterface")));
   UseInterface(i);
 }
 
-void radioProtocol_clicked(GtkWidget *widget, gpointer user_data)
-{
+void radioProtocol_clicked(GtkWidget *widget, gpointer user_data) {
   //NOTE: 3vil hax and only verks with 2 toggles :)
   if(!gtk_toggle_button_get_active ((GtkToggleButton*)widget)) {
     UseUDP(!udp_hax);
@@ -65,8 +62,7 @@ void radioProtocol_clicked(GtkWidget *widget, gpointer user_data)
   }
 }
 
-uint32 show_gooey()
-{
+uint32 show_gooey() {
   GtkWidget *widget;
 
   /* load the interface from the xml buffer */
@@ -77,76 +73,77 @@ uint32 show_gooey()
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `btnSendMessage'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSendMessage_clicked), NULL);
-    }
+  }
 
   widget = glade_xml_get_widget (main_window, "btnReceive");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `btnReceive'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnReceive_clicked), NULL);
-    }
+  }
 
   widget = glade_xml_get_widget (main_window, "btnSetPort");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `btnSetPort'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetPort_clicked), NULL);
-    }
+  }
 
   widget = glade_xml_get_widget (main_window, "btnSetInterface");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `btnSetInterface'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetInterface_clicked), NULL);
-    }
+  }
 
   widget = glade_xml_get_widget (main_window, "radioProtocolIPX");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `radioProtocolIPX'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
-    }
+  }
 
   widget = glade_xml_get_widget (main_window, "radioProtocolUDP");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `radioProtocolUDP'!\n");
     return -1;
-    }
+  }
   else {
     g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
-    }
+  }
+
   //Set DnD targets
   widget = glade_xml_get_widget (main_window, "tvMyShares");
   if(widget==NULL) {
     fprintf(stderr,"Error: can not find widget `tvMyShares'!\n");
     return -1;
-    }
+  }
   else {
-    static GtkTargetEntry target_table[] = {{ }};
+    static GtkTargetEntry target_table[] = {{ }}; //I don't know any mime-types :(
     gtk_drag_dest_set(widget, GTK_DEST_DEFAULT_ALL,
                       target_table, 0, GDK_ACTION_COPY);
-    gtk_drag_dest_add_uri_targets(widget);
+    gtk_drag_dest_add_uri_targets(widget);        //luckily GTK provides this handy function :)
     gtk_signal_connect (GTK_OBJECT (widget), "drag_data_received",
                         GTK_SIGNAL_FUNC (tvMySharestarget_drag_data_received),
                         NULL);
-    }
+  }
 
   /* Have the delete event (window close) end the program */
   widget = glade_xml_get_widget (main_window, "frmMain");
   g_signal_connect (G_OBJECT (widget), "delete_event", G_CALLBACK (gtk_main_quit), NULL);
 
-  /* start the event loop */
+  /* start the event loop. TODO: Do this in the background? */
   gtk_main ();
   return true;
 }
