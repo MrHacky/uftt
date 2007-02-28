a73ed6cad1221adf782c3675f1fc9efaa50d098d
simon.sasburg <mrhacky@users.noreply.github.com>
2007-02-28 20:10:30 +0000

    [make] improve generation of gladebuf.cpp so it plays nice with kdevelop

--- src/gladegen.sh
+++ src/gladegen.sh
@@ -1,10 +1,7 @@
-rm -f gladebuf.cpp
-touch gladebuf.cpp
-echo "#include \"stdafx.h\"" >> gladebuf.cpp
-echo "#include \"gladebuf.h\"" >> gladebuf.cpp
-echo "char gladebuf[] =" >> gladebuf.cpp
-#echo \"\\ >> gladebuf.cpp
-cat uftt.glade | sed s:\\\\:\\\\\\\\:g | sed s:\":\\\\\":g | sed s:$:\": | sed s:^:\": >> gladebuf.cpp
-#echo \" >> gladebuf.cpp
-echo ";" >> gladebuf.cpp
-echo "int gladebufsize = sizeof(gladebuf);" >> gladebuf.cpp
+echo "#include \"stdafx.h\""
+echo "#include \"gladebuf.h\""
+echo "char gladebuf[] ="
+cat ${1} | sed s:\\\\:\\\\\\\\:g | sed s:\":\\\\\":g | sed s:$:\": | sed s:^:\":
+echo ";"
+echo "int gladebufsize = sizeof(gladebuf);"
+
