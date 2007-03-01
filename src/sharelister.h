9fdaec4fc6ed8fabce6e4c293e66fb3326df9e30
daniel.geelen <daniel.geelen@c71387f0-5629-0410-9bdc-e9ce6186880e>
2007-03-01 12:48:25 +0000

    [oops] sharelister vergeten

---
+++ src/sharelister.h
@@ -0,0 +1,44 @@
+#ifndef SHARELISTER_H
+#define SHARELISTER_H
+
+#include "stdafx.h"
+
+enum {
+	COL_NAME = 0,
+	COL_AGE,
+	NUM_COLS
+} ;
+
+struct FileInfo {
+	std::string name;
+	uint32 attrs;
+	std::vector<FileInfo*> file;
+	uint64 UID;
+};
+
+struct ShareInfo {
+	std::string name;
+	FileInfo* share;
+	uint64 UID;
+};
+
+struct ServerInfo {
+	std::string name;
+	sockaddr* address;
+	std::vector<ShareInfo*> share;
+	uint64 UID;
+};
+
+GtkTreeModel * WINAPI
+create_and_fill_model( void );
+
+GtkWidget * WINAPI
+create_view_and_model( GtkTreeView* aview );
+
+void add_server_to_list(struct ServerInfo);
+void remove_server_from_list(uint64 UID);
+void add_share_to_server(uint64 UID, struct ShareInfo share);
+void remove_share_from_server(uint64 serverUID, uint64 shareUID);
+void add_file_to_share(uint64 shareUID, struct FileInfo file);
+void remove_file_from_share(uint64 shareUID, uint64 fileUID);
+#endif //SHARELISTER_H
