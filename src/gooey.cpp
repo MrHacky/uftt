59f22e530810c08a4e90808b4aba9399ca292034
simon.sasburg <mrhacky@users.noreply.github.com>
2007-03-03 09:30:28 +0000

    ident fixing

--- src/gooey.cpp
+++ src/gooey.cpp
@@ -1,8 +1,8 @@
 /**------------------------------------------------------------------------**
- * NOTE:
- * It is my intention to only have the bindings/connections in this file,
- * and call the appropriate routines in other files.
- **------------------------------------------------------------------------**/
+* NOTE:
+* It is my intention to only have the bindings/connections in this file,
+* and call the appropriate routines in other files.
+**------------------------------------------------------------------------**/
 
 #include "stdafx.h"
 
@@ -20,15 +20,15 @@ extern bool udp_hax;
 extern ServerInfo * myServer;
 
 void tvMyShares_target_drag_data_received(GtkWidget          *widget,
-                                        GdkDragContext     *context,
-                                        gint                x,
-                                        gint                y,
-                                        GtkSelectionData   *data,
-                                        guint               info,
-                                        guint               time){
-  fprintf(stdout,"tvMyShares_target_drag_data_received:\nDATA=%i:%s\n",info,data->data);
-  /* TODO: 'Add URI to sharelist TreeView' (prob. requires making a ModelView for the TreeView)*/
-  char *i = (char*)data->data;
+																				GdkDragContext     *context,
+																				gint                x,
+																				gint                y,
+																				GtkSelectionData   *data,
+																				guint               info,
+																				guint               time){
+	fprintf(stdout,"tvMyShares_target_drag_data_received:\nDATA=%i:%s\n",info,data->data);
+	/* TODO: 'Add URI to sharelist TreeView' (prob. requires making a ModelView for the TreeView)*/
+	char *i = (char*)data->data;
 	while(*i!=0) {
 		char *j = i;
 		string str;
@@ -41,214 +41,213 @@ void tvMyShares_target_drag_data_received(GtkWidget          *widget,
 			myServer->add_share(share);
 		}
 		i+=2;
-  }
-  //
+	}
+	//
 
-  g_signal_stop_emission_by_name(widget,"drag_data_received"); // Don't know if this is correct, but it makes GTK STFU
+	g_signal_stop_emission_by_name(widget,"drag_data_received"); // Don't know if this is correct, but it makes GTK STFU
 }
 
 void btnSendMessage_clicked(GtkWidget *widget, gpointer user_data) {
-  string s=gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSendMessage"));
-  send_msg( s, port );
+	string s=gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSendMessage"));
+	send_msg( s, port );
 }
 
 void btnReceive_clicked(GtkWidget *widget, gpointer user_data) {
-  gtk_widget_set_sensitive(widget, false);
-  string msg, from;
+	gtk_widget_set_sensitive(widget, false);
+	string msg, from;
 
-  if( recv_msg(msg, port, &from) != SOCKET_ERROR) { //FIXME: What about other errors? UNIX way is better: '>= 0'
-    msg = "received: '" + msg + "' from: " + from;
-    gtk_entry_set_text((GtkEntry*) glade_xml_get_widget (main_window, "txtLastReceived"), msg.c_str());
-  }
-  gtk_widget_set_sensitive(widget, true);
+	if( recv_msg(msg, port, &from) != SOCKET_ERROR) {
+		msg = "received: '" + msg + "' from: " + from;
+		gtk_entry_set_text((GtkEntry*) glade_xml_get_widget (main_window, "txtLastReceived"), msg.c_str());
+	}
+	gtk_widget_set_sensitive(widget, true);
 }
 
 void btnSetPort_clicked(GtkWidget *widget, gpointer user_data) {
-  int p = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSetPort")));
-  UsePort(p);
+	int p = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtSetPort")));
+	UsePort(p);
 }
 
 void btnSetInterface_clicked(GtkWidget *widget, gpointer user_data) {
-  int i = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtInterface")));
-  UseInterface(i);
+	int i = atoi(gtk_entry_get_text((GtkEntry*) glade_xml_get_widget (main_window, "txtInterface")));
+	UseInterface(i);
 }
 
 void radioProtocol_clicked(GtkWidget *widget, gpointer user_data) {
-  //NOTE: 3vil hax and only verks with 2 toggles :)
-  if(!gtk_toggle_button_get_active ((GtkToggleButton*)widget)) {
-    UseUDP(!udp_hax);
-    fprintf(stderr, "SWITCH: udp_hax=%s\n",udp_hax?"true":"false");
-  }
+	//NOTE: 3vil hax and only verks with 2 toggles :)
+	if(!gtk_toggle_button_get_active ((GtkToggleButton*)widget)) {
+		UseUDP(!udp_hax);
+		fprintf(stderr, "SWITCH: udp_hax=%s\n",udp_hax?"true":"false");
+	}
 }
 
 void SpamSpamCallback(uint32 count) {
-  static GtkWidget *widget = NULL;
-  if(widget==NULL) {
-    widget = glade_xml_get_widget (main_window, "txtSpamSpammed");
-  }
-  if(widget!=NULL) {
-    char str[128];
-    snprintf(str, 127, "%lu",count);
-    gtk_entry_set_text((GtkEntry*)widget, str);
-    gtk_widget_queue_draw(widget);  // force a redraw of the widget
-  }
+	static GtkWidget *widget = NULL;
+	if(widget==NULL) {
+		widget = glade_xml_get_widget (main_window, "txtSpamSpammed");
+	}
+	if(widget!=NULL) {
+		char str[128];
+		snprintf(str, 127, "%lu",count);
+		gtk_entry_set_text((GtkEntry*)widget, str);
+		gtk_widget_queue_draw(widget);  // force a redraw of the widget
+	}
 }
 
 THREAD SpamSpamThread;
 void btnSpamSpam_clicked(GtkWidget *widget, gpointer user_data) {
-  extern bool SpamMoreSpam;
-  if(!SpamMoreSpam) {
-    SpamSpamArgs *Args = new SpamSpamArgs;
-    Args->s = "SpamSpam";
-    Args->p = SpamSpamCallback;
-    SpamSpamThread = spawnThread(SpamSpam, Args);
-    gtk_button_set_label((GtkButton*)widget, "Stop Spammming!");
-  }
-  else {
-    //Stop Spamming
-    SpamMoreSpam=false;
-    gtk_button_set_label((GtkButton*)widget, "Spam Spam!");
-  }
+	extern bool SpamMoreSpam;
+	if(!SpamMoreSpam) {
+		SpamSpamArgs *Args = new SpamSpamArgs;
+		Args->s = "SpamSpam";
+		Args->p = SpamSpamCallback;
+		SpamSpamThread = spawnThread(SpamSpam, Args);
+		gtk_button_set_label((GtkButton*)widget, "Stop Spammming!");
+	}
+	else {
+		//Stop Spamming
+		SpamMoreSpam=false;
+		gtk_button_set_label((GtkButton*)widget, "Spam Spam!");
+	}
 }
 
 void ReceiveSpamCallback(uint32 count) {
-  static GtkWidget *widget = NULL;
-  if(widget==NULL) {
-    widget = glade_xml_get_widget (main_window, "txtSpamReceived");
-  }
-  if(widget!=NULL) {
-    char str[128];
-    snprintf(str, 127, "%lu",count);
-    gtk_entry_set_text((GtkEntry*)widget, str);
-    gtk_widget_queue_draw(widget);  // force a redraw of the widget
-  }
+	static GtkWidget *widget = NULL;
+	if(widget==NULL) {
+		widget = glade_xml_get_widget (main_window, "txtSpamReceived");
+	}
+	if(widget!=NULL) {
+		char str[128];
+		snprintf(str, 127, "%lu",count);
+		gtk_entry_set_text((GtkEntry*)widget, str);
+		gtk_widget_queue_draw(widget);  // force a redraw of the widget
+	}
 }
 
 THREAD ReceiveSpamThread;
 void btnReceiveSpam_clicked(GtkWidget *widget, gpointer user_data) {
-  extern bool ReceiveMoreSpam;
-  if(!ReceiveMoreSpam) {
-    SpamSpamArgs *Args = new SpamSpamArgs;
-    Args->s = ""; //not used
-    Args->p = ReceiveSpamCallback;
-    //ReceiveSpamThread = spawnThread((int* (WINAPI*)(x*))ReceiveSpam, (x*)Args);
-    gtk_button_set_label((GtkButton*)widget, "Stop Receiving!");
-  }
-  else {
-    //Stop Spamming
-    ReceiveMoreSpam=false;
-    gtk_button_set_label((GtkButton*)widget, "Receive Spam!");
-  }
+	extern bool ReceiveMoreSpam;
+	if(!ReceiveMoreSpam) {
+		SpamSpamArgs *Args = new SpamSpamArgs;
+		Args->s = ""; //not used
+		Args->p = ReceiveSpamCallback;
+		//ReceiveSpamThread = spawnThread((int* (WINAPI*)(x*))ReceiveSpam, (x*)Args);
+		gtk_button_set_label((GtkButton*)widget, "Stop Receiving!");
+	}
+	else {
+		//Stop Spamming
+		ReceiveMoreSpam=false;
+		gtk_button_set_label((GtkButton*)widget, "Receive Spam!");
+	}
 }
 
 uint32 show_gooey() {
-  GtkWidget *widget;
+	GtkWidget *widget;
 
-  /* load the interface from the xml buffer */
-  main_window = glade_xml_new_from_buffer(gladebuf, gladebufsize, NULL, NULL);
+	/* load the interface from the xml buffer */
+	main_window = glade_xml_new_from_buffer(gladebuf, gladebufsize, NULL, NULL);
 
-  /* connect the signals in the interface */
-  widget = glade_xml_get_widget (main_window, "btnSendMessage");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnSendMessage'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSendMessage_clicked), NULL);
-  }
+	/* connect the signals in the interface */
+	widget = glade_xml_get_widget (main_window, "btnSendMessage");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnSendMessage'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSendMessage_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "btnReceive");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnReceive'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnReceive_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "btnReceive");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnReceive'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnReceive_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "btnSetPort");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnSetPort'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetPort_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "btnSetPort");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnSetPort'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetPort_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "btnSetInterface");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnSetInterface'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetInterface_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "btnSetInterface");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnSetInterface'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSetInterface_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "radioProtocolIPX");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `radioProtocolIPX'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "radioProtocolIPX");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `radioProtocolIPX'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "radioProtocolUDP");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `radioProtocolUDP'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "radioProtocolUDP");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `radioProtocolUDP'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (radioProtocol_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "btnSpamSpam");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnSpamSpam'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSpamSpam_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "btnSpamSpam");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnSpamSpam'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnSpamSpam_clicked), NULL);
+	}
 
-  widget = glade_xml_get_widget (main_window, "btnReceiveSpam");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `btnReceiveSpam'!\n");
-    return -1;
-  }
-  else {
-    g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnReceiveSpam_clicked), NULL);
-  }
+	widget = glade_xml_get_widget (main_window, "btnReceiveSpam");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `btnReceiveSpam'!\n");
+		return -1;
+	}
+	else {
+		g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (btnReceiveSpam_clicked), NULL);
+	}
 
-  //Set DnD targets
-  widget = glade_xml_get_widget (main_window, "tvMyShares");
-  if(widget==NULL) {
-    fprintf(stderr,"Error: can not find widget `tvMyShares'!\n");
-    return -1;
-  }
-  else {
-    static const GtkTargetEntry target_table[] = { {} };
-    gtk_drag_dest_set(	widget, \
-		                GTK_DEST_DEFAULT_ALL,\
-						target_table, \
-						0, \
-//							(GdkDragAction)(GDK_ACTION_COPY | GDK_ACTION_MOVE)
-						GDK_ACTION_MOVE //FIXME: possibly (KDE vs Gnome): (GdkDragAction)(GDK_ACTION_COPY | GDK_ACTION_MOVE)
-                     );
-    gtk_drag_dest_add_uri_targets(widget);
-    gtk_signal_connect (GTK_OBJECT (widget),
-	                    "drag_data_received",
-                        GTK_SIGNAL_FUNC (tvMyShares_target_drag_data_received),
-                        NULL);
-	create_view_and_model((GtkTreeView*)widget);
-  }
+	//Set DnD targets
+	widget = glade_xml_get_widget (main_window, "tvMyShares");
+	if(widget==NULL) {
+		fprintf(stderr,"Error: can not find widget `tvMyShares'!\n");
+		return -1;
+	}
+	else {
+		static const GtkTargetEntry target_table[] = { {} };
+		gtk_drag_dest_set(widget, \
+											GTK_DEST_DEFAULT_ALL,\
+											target_table, \
+											0, \
+											GDK_ACTION_MOVE //FIXME: possibly (KDE vs Gnome): (GdkDragAction)(GDK_ACTION_COPY | GDK_ACTION_MOVE)
+											);
+		gtk_drag_dest_add_uri_targets(widget);
+		gtk_signal_connect (GTK_OBJECT (widget),
+												"drag_data_received",
+												GTK_SIGNAL_FUNC (tvMyShares_target_drag_data_received),
+												NULL);
+		create_view_and_model((GtkTreeView*)widget);
+	}
 
-  /* Have the delete event (window close) end the program */
-  widget = glade_xml_get_widget (main_window, "frmMain");
-  g_signal_connect (G_OBJECT (widget), "delete_event", G_CALLBACK (gtk_main_quit), NULL);
+	/* Have the delete event (window close) end the program */
+	widget = glade_xml_get_widget (main_window, "frmMain");
+	g_signal_connect (G_OBJECT (widget), "delete_event", G_CALLBACK (gtk_main_quit), NULL);
 
-  /* start the event loop. TODO: Do this in the background? */
-  //spawnThread(gtk_main(), NULL); ?
-  gtk_main ();
-  return true;
+	/* start the event loop. TODO: Do this in the background? */
+	//spawnThread(gtk_main(), NULL); ?
+	gtk_main ();
+	return true;
 }
--- src/sharelister.cpp
+++ src/sharelister.cpp
@@ -15,12 +15,12 @@ void init_server_list(){
 }
 
 FileInfo::FileInfo(std::string path) {
-  fprintf(stderr,"FileInfo: Traversing %s\n", path.c_str());
+	fprintf(stderr,"FileInfo: Traversing %s\n", path.c_str());
 	DIR *dir = opendir(path.c_str());
 	if(dir!=NULL){ // is dir
 		dirent *fent; // file entry
 		while((fent=readdir (dir))!=NULL) {
-		  if(string(fent->d_name) != "." && string(fent->d_name) != "..") {
+			if(string(fent->d_name) != "." && string(fent->d_name) != "..") {
 				file.push_back(new FileInfo(path+"/"+fent->d_name));
 			}
 		}
@@ -32,90 +32,90 @@ FileInfo::FileInfo(std::string path) {
 
 ShareInfo::ShareInfo(std::string path) {
 	fprintf(stderr,"Parsing %s\n",path.c_str());
-  root = new FileInfo(path.c_str());
+	root = new FileInfo(path.c_str());
 }
 
 void ServerInfo::add_share(ShareInfo* share)
 {
-  //TODO: void add_share_to_server(uint64 UID, const struct ShareInfo &share);
-  fprintf(stderr,"TODO: Adding share to server\n");
+	//TODO: void add_share_to_server(uint64 UID, const struct ShareInfo &share);
+	fprintf(stderr,"TODO: Adding share to server\n");
 }
 
 GtkTreeModel * WINAPI
 create_and_fill_model (void)
 {
-  GtkTreeStore  *store;
-  GtkTreeIter    iter;
+	GtkTreeStore  *store;
+	GtkTreeIter    iter;
 	GtkTreeIter    iter2;
 
-  store = gtk_tree_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_UINT);
+	store = gtk_tree_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_UINT);
 
 
-  /* Append a row and fill in some data */
-  gtk_tree_store_append (store, &iter, NULL);
-  gtk_tree_store_set (store, &iter,
-                      COL_NAME, "Heinz El-Mann",
-                      COL_AGE, 51,
-                      -1);
+	/* Append a row and fill in some data */
+	gtk_tree_store_append (store, &iter, NULL);
+	gtk_tree_store_set (store, &iter,
+											COL_NAME, "Heinz El-Mann",
+											COL_AGE, 51,
+											-1);
 
-  iter2 = iter;
-  /* append another row and fill in some data */
-  gtk_tree_store_append (store, &iter, &iter2);
-  gtk_tree_store_set (store, &iter,
-                      COL_NAME, "Jane Doe",
-                      COL_AGE, 23,
-                      -1);
+	iter2 = iter;
+	/* append another row and fill in some data */
+	gtk_tree_store_append (store, &iter, &iter2);
+	gtk_tree_store_set (store, &iter,
+											COL_NAME, "Jane Doe",
+											COL_AGE, 23,
+											-1);
 
-  /* ... and a third row */
-  iter2 = iter;
-  gtk_tree_store_append (store, &iter, &iter2);
-  gtk_tree_store_set (store, &iter,
-                      COL_NAME, "Joe Bungop",
-                      COL_AGE, 91,
-                      -1);
+	/* ... and a third row */
+	iter2 = iter;
+	gtk_tree_store_append (store, &iter, &iter2);
+	gtk_tree_store_set (store, &iter,
+											COL_NAME, "Joe Bungop",
+											COL_AGE, 91,
+											-1);
 
-  return GTK_TREE_MODEL (store);
+	return GTK_TREE_MODEL (store);
 }
 
 GtkWidget * WINAPI
 create_view_and_model (GtkTreeView* aview)
 {
-  GtkCellRenderer     *renderer;
-  GtkTreeModel        *model;
-  GtkWidget           *view = (GtkWidget*)aview;
+	GtkCellRenderer     *renderer;
+	GtkTreeModel        *model;
+	GtkWidget           *view = (GtkWidget*)aview;
 
-  //view = gtk_tree_view_new ();
+	//view = gtk_tree_view_new ();
 
-  /* --- Column #1 --- */
+	/* --- Column #1 --- */
 
-  renderer = gtk_cell_renderer_text_new ();
-  gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
-                                               -1,
-                                               "Name",
-                                               renderer,
-                                               "text", COL_NAME,
-                                               NULL);
+	renderer = gtk_cell_renderer_text_new ();
+	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
+																							-1,
+																							"Name",
+																							renderer,
+																							"text", COL_NAME,
+																							NULL);
 
-  /* --- Column #2 --- */
+	/* --- Column #2 --- */
 
-  renderer = gtk_cell_renderer_text_new ();
-  gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
-                                               -1,
-                                               "Age",
-                                               renderer,
-                                               "text", COL_AGE,
-                                               NULL);
+	renderer = gtk_cell_renderer_text_new ();
+	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
+																							-1,
+																							"Age",
+																							renderer,
+																							"text", COL_AGE,
+																							NULL);
 
-  model = create_and_fill_model ();
+	model = create_and_fill_model ();
 
-  gtk_tree_view_set_model (GTK_TREE_VIEW (view), model);
+	gtk_tree_view_set_model (GTK_TREE_VIEW (view), model);
 
-  /* The tree view has acquired its own reference to the
-   *  model, so we can drop ours. That way the model will
-   *  be freed automatically when the tree view is destroyed */
+	/* The tree view has acquired its own reference to the
+	*  model, so we can drop ours. That way the model will
+	*  be freed automatically when the tree view is destroyed */
 
-  g_object_unref (model);
+	g_object_unref (model);
 
-  return view;
+	return view;
 }
 
