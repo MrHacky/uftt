29028312ff53a95df711a9af480fbded1cda1b4a
daniel.geelen <daniel.geelen@c71387f0-5629-0410-9bdc-e9ce6186880e>
2007-03-28 12:26:28 +0000

    [SharelisterGUI] There is no spork

--- src/sharelister_gui.cpp
+++ src/sharelister_gui.cpp
@@ -80,11 +80,64 @@ add_tree_data(  GtkTreeView* aview, ShareInfo* share) {
 }
 
 GtkWidget * WINAPI
-init_tree_view (GtkTreeView* aview)
-{
-	GtkCellRenderer     *renderer;
+init_tree_view (GtkTreeView* aview) {
 	GtkTreeModel        *model;
 	GtkWidget           *view = (GtkWidget*)aview;
+	GtkCellRenderer     *renderer = gtk_cell_renderer_pixbuf_new ();;
+	GdkPixbuf* p1 = gdk_pixbuf_new_from_xpm_data((const char**)folder_open_xpm);
+	GdkPixbuf* p2 = gdk_pixbuf_new_from_xpm_data((const char**)folder_closed_xpm);
+	g_object_set(renderer,"pixbuf-expander-open",p1,NULL);
+	g_object_set(renderer,"pixbuf-expander-closed",p2,NULL);
+
+	GtkTreeViewColumn *column = gtk_tree_view_column_new_with_attributes("Name", renderer);
+	gtk_tree_view_column_pack_start (column, renderer, FALSE);
+	gtk_tree_view_column_set_attributes (column, renderer,
+	                                     "pixbuf-expander-closed", p2,
+	                                     "pixbuf-expander-open", p1,
+	                                     NULL);
+	renderer = gtk_cell_renderer_text_new ();
+	gtk_tree_view_column_pack_start (column, renderer, TRUE);
+	gtk_tree_view_column_set_attributes (column, renderer,
+					     "text", COL_NAME,
+					     NULL);
+	gtk_tree_view_insert_column(GTK_TREE_VIEW (view), column, -1);
+
+	renderer = gtk_cell_renderer_text_new ();
+	gtk_tree_view_insert_column_with_attributes (GTK_TREE_VIEW (view),
+	                                             -1,
+	                                             "Size",
+	                                             renderer,
+	                                             "text", COL_SIZE,
+	                                             NULL);
+
+	model = GTK_TREE_MODEL (gtk_tree_store_new (NUM_COLS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING));
+	gtk_tree_view_set_model (GTK_TREE_VIEW (view), model);
+
+	GValue *\
+	gval = g_new0 (GValue, 1);
+	g_value_init (gval, G_TYPE_BOOLEAN);
+	g_value_set_boolean (gval, true);
+	g_object_set_property (G_OBJECT (aview), "enable-tree-lines", gval);
+	g_free (gval);
+
+	gval = g_new0 (GValue, 1);
+	g_value_init (gval, G_TYPE_BOOLEAN);
+	g_value_set_boolean (gval, true);
+	g_object_set_property (G_OBJECT (aview), "show-expanders", gval);
+	g_free (gval);
+
+	gval = g_new0 (GValue, 1);
+	g_value_init (gval, G_TYPE_INT);
+	g_value_set_int (gval, 0);
+	g_object_set_property (G_OBJECT (aview), "level-indentation", gval);
+	g_free (gval);
+
+	g_object_unref (model);
+	return view;
+
+
+/******************************************************************************************************************/
+
 
 	renderer = gtk_cell_renderer_pixbuf_new ();
 
@@ -98,7 +151,7 @@ init_tree_view (GtkTreeView* aview)
 			//p = gdk_pixbuf_new_from_file("../../src/folder_closed.gif", &err);
 			//g_object_set(renderer,"pixbuf",p,NULL);
 /*/
-	GdkPixbuf* p1,*p2;
+/*	GdkPixbuf* p1,*p2;
 	GError *err = 0;
 	p1 = gdk_pixbuf_new_from_xpm_data((const char**)folder_open_xpm);
 	fprintf(stderr, "ERROR = %i / \n",err);
@@ -114,7 +167,7 @@ init_tree_view (GtkTreeView* aview)
 /*********************************************************************/
 
 
-	GtkTreeViewColumn*  column ;/*= gtk_tree_view_column_new_with_attributes(
+/*	GtkTreeViewColumn*  column ;/*= gtk_tree_view_column_new_with_attributes(
 			"Name",
 			renderer,
 			NULL);
@@ -124,6 +177,7 @@ init_tree_view (GtkTreeView* aview)
 	gtk_tree_view_column_pack_start (column, renderer, FALSE);
 
 //	g_object_set_property (G_OBJECT (aview), "expander-column", (GValue*)collumn);
+
 
 	gtk_tree_view_column_set_attributes (column, renderer,
 					     "pixbuf-expander-closed", p2,
@@ -164,7 +218,7 @@ init_tree_view (GtkTreeView* aview)
 
 	gtk_tree_view_set_model (GTK_TREE_VIEW (view), model);
 
-	GValue *gval = g_new0 (GValue, 1);
+//	GValue *gval = g_new0 (GValue, 1);
 	g_value_init (gval, G_TYPE_BOOLEAN);
 	g_value_set_boolean (gval, true);
 	g_object_set_property (G_OBJECT (aview), "enable-tree-lines", gval);
