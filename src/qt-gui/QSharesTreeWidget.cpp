f9e080325fac1be464b861e321d3f6c4a333d1f5
Simon Sasburg <simon.sasburg@gmail.com>
2007-08-05 18:34:06 +0000

    drag 'n drop start

--- src/qt-gui/QSharesTreeWidget.cpp
+++ src/qt-gui/QSharesTreeWidget.cpp
@@ -1,6 +1,33 @@
 #include "QSharesTreeWidget.h"
 
+//#include <QImageDrag>
+//#include <QTextDrag>
+
+#include <iostream>
+
+using namespace std;
+
 QSharesTreeWidget::QSharesTreeWidget(QWidget*& widget)
 	: QTreeWidget(widget)
 {
+	setAcceptDrops(true);
 };
+
+void QSharesTreeWidget::dragEnterEvent(QDragEnterEvent* event)
+{
+	cout << "event\n";
+	if (event->mimeData()->hasFormat("text/plain"))
+		event->acceptProposedAction();
+}
+
+void QSharesTreeWidget::dragMoveEvent(QDragMoveEvent* event)
+{
+	QWidget::dragMoveEvent(event);
+}
+
+void QSharesTreeWidget::dropEvent(QDropEvent* event)
+{
+	
+	cout << "try\n" << event->mimeData()->text().toStdString() << '\n';
+	event->acceptProposedAction();
+}
--- src/qt-gui/QSharesTreeWidget.h
+++ src/qt-gui/QSharesTreeWidget.h
@@ -2,10 +2,14 @@
 #define QSHARETREEWIDGET_H
 
 #include <QTreeWidget>
+#include <QDragEnterEvent>
 
 class QSharesTreeWidget: public QTreeWidget {
 	public:
 		QSharesTreeWidget(QWidget*& widget);
+		void dragMoveEvent(QDragMoveEvent* event);
+		void dragEnterEvent(QDragEnterEvent *event);
+		void dropEvent(QDropEvent* event);
 };
 
 #endif
