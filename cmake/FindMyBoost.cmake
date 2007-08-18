19b3adc9768ee6b462793a0276378ff47d1c0206
Simon Sasburg <simon.sasburg@gmail.com>
2007-08-18 10:41:26 +0000

    get cmake and boost to work in windows

--- cmake/FindMyBoost.cmake
+++ cmake/FindMyBoost.cmake
@@ -9,26 +9,39 @@
 
 FIND_PATH(BOOST_INCLUDE_DIR
 	NAMES boost/config.hpp
-	PATHS /usr/include/boost /usr/local/include/boost
+	PATHS
+		/usr/include/boost /usr/local/include/boost
+		"C:/Program Files/boost/boost_1_34_1"
+		"C:/Program Files/boost/boost_1_34_0"
+		"C:/Program Files/boost/boost_1_34"
 )
 
-IF (NOT BOOST_LIBRARY_SUFFIX)
-	MESSAGE(STATUS "No BOOST_LIBRARY_SUFFIX")
-	# TODO: autodetect BOOST_LIBRARY_SUFFIX somehow
-ENDIF (NOT BOOST_LIBRARY_SUFFIX)
+IF(WIN32)
+	# depend on autolinking on windows
+	SET(BOOST_THREAD_LIBRARY "")
+	SET(BOOST_FILESYSTEM_LIBRARY "")
+	SET(BOOST_LIBRARY_DIR "${BOOST_INCLUDE_DIR}/lib")
+ELSE(WIN32)
 
-SET(BOOST_LIBRARY_SUFFIX
-	-gcc41-mt
-	CACHE STRING
-	"suffix to use for boost libraries"
-)
+	IF (NOT BOOST_LIBRARY_SUFFIX)
+		MESSAGE(STATUS "No BOOST_LIBRARY_SUFFIX")
+		# TODO: autodetect BOOST_LIBRARY_SUFFIX somehow
+	ENDIF (NOT BOOST_LIBRARY_SUFFIX)
 
-FIND_LIBRARY(BOOST_THREAD_LIBRARY
-	NAMES boost_thread-mt boost_thread${BOOST_LIBRARY_SUFFIX}
-	PATH /usr/lib /usr/local/lib
-)
+	SET(BOOST_LIBRARY_SUFFIX
+		-gcc41-mt
+		CACHE STRING
+		"suffix to use for boost libraries"
+	)
+	
+	FIND_LIBRARY(BOOST_THREAD_LIBRARY
+		NAMES boost_thread-mt boost_thread${BOOST_LIBRARY_SUFFIX}
+		PATH /usr/lib /usr/local/lib
+	)
 
-FIND_LIBRARY(BOOST_FILESYSTEM_LIBRARY
-	NAMES boost_filesystem-mt boost_filesystem${BOOST_LIBRARY_SUFFIX}
-	PATH /usr/lib /usr/local/lib
-)
+	FIND_LIBRARY(BOOST_FILESYSTEM_LIBRARY
+		NAMES boost_filesystem-mt boost_filesystem${BOOST_LIBRARY_SUFFIX}
+		PATH /usr/lib /usr/local/lib
+	)
+
+ENDIF(WIN32)
