f3cba56410b9214d6b99d34a67d8b306775afc5f
Simon Sasburg <simon.sasburg@gmail.com>
2008-06-15 10:11:52 +0000

    Strip path from logged files for now.

--- src/Logger.h
+++ src/Logger.h
@@ -3,6 +3,14 @@
 
 #include <iostream>
 
-#define LOG(msg) std::cout << __FILE__ << ':' << __LINE__ << ':' << msg << endl
+inline char* nameonly(char* str)
+{
+	for (char* loop = str; *loop; ++loop)
+		if (*loop == '\\') str = loop;
+
+	return str;
+}
+
+#define LOG(msg) std::cout << nameonly(__FILE__) << ':' << __LINE__ << ':' << msg << endl
 
 #endif//LOGGER_H
