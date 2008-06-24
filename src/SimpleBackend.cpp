ec4f34d260c97be0471d3d79e9f99cde8c8de7be
Simon Sasburg <simon.sasburg@gmail.com>
2008-06-24 21:36:09 +0000

    Move lot of stuff to SimpleBackend.h (and .cpp)

---
+++ src/SimpleBackend.cpp
@@ -0,0 +1,11 @@
+#include "SimpleBackend.h"
+
+void SimpleTCPConnection::getsharepath(std::string sharename)
+{
+	backend->getsharepath(this, sharename);
+}
+
+void SimpleTCPConnection::sig_download_ready(std::string url)
+{
+	backend->sig_download_ready(url);
+}
