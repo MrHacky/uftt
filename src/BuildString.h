badee340e8338f73131e525f167aaeb49abe76d1
Simon Sasburg <simon.sasburg@gmail.com>
2008-09-06 13:49:29 +0000

    Detect linux for buildstring.

--- src/BuildString.h
+++ src/BuildString.h
@@ -9,6 +9,8 @@
 #if defined(WIN32) || defined(__WIN32__)
 //#define BUILD_STRING_PLATFORM "win32"
 #define BUILD_STRING_PLATFORM "win32.nt"
+#elif defined(__linux__)
+#define BUILD_STRING_PLATFORM "linux.i386"
 #else
 #define BUILD_STRING_PLATFORM "unknown"
 #endif
@@ -21,9 +23,13 @@
 
 #include <BuildVersion.h>
 
+#ifndef BUILD_STRING_EXTRA
+#define BUILD_STRING_EXTRA ""
+#endif
+
 #define BUILD_STRING_VERSION "-" VERSION_MAJOR_STRING "." VERSION_MINOR_STRING "." VERSION_REV_STRING
 
-#define BUILD_STRING ("uftt-" BUILD_STRING_PLATFORM BUILD_STRING_LINKAGE BUILD_STRING_VERSION)
+#define BUILD_STRING ("uftt-" BUILD_STRING_PLATFORM BUILD_STRING_LINKAGE BUILD_STRING_EXTRA BUILD_STRING_VERSION)
 
 extern char build_string_macro_date[];
 extern char build_string_macro_time[];
