4f7043d3cfa9f4c109ba2e2412f8e897d802cebb
Simon Sasburg <simon.sasburg@gmail.com>
2008-09-20 09:56:04 +0000

    Add dummy bench4.

---
+++ src/bench/Bench4.cpp
@@ -0,0 +1,23 @@
+#if 0
+#include <windows.h>
+
+int APIENTRY synesisWinMain(HINSTANCE,
+                            HINSTANCE,
+                            LPSTR,
+                            int)
+{
+    int iRet;
+/*
+    iRet = WinMain( GetModuleHandle(NULL),
+                    NULL,
+                    GetAdjustedCommandLine(),
+                    GetWindowShowState());
+
+    trace_leaks();
+*/
+    return iRet;
+}
+#endif
+int main( int argc, char **argv ) {
+
+}
