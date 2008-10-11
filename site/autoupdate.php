59d6d319d0e2aa48d3814e531a4616bf152bd646
Simon Sasburg <simon.sasburg@gmail.com>
2008-10-11 19:18:30 +0000

    Move site/ to better location.

---
+++ site/autoupdate.php
@@ -0,0 +1,30 @@
+<div style="display:none;">
+-----BEGIN RSA CHECKED CONTENT-----
+</div>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r401.exe">uftt-win32.nt-static-1.22.401</a>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-9x-r401.exe">uftt-win32.9x-static-1.22.401</a>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r399.exe">uftt-win32.nt-static-1.20.399</a>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-9x-r399.exe">uftt-win32.9x-static-1.20.399</a>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r372.exe">uftt-win32.nt-static-1.18.372</a>
+<br>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r343.exe">uftt-win32.nt-static-1.16.343</a>
+<br>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r320.exe">uftt-win32.nt-static-1.12.320</a>
+<br>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r273.exe">uftt-win32.nt-static-1.10.273</a>
+<br>
+<a href="http://hackykid.heliohost.org/site/files/uftt-0.1.343-Linux.deb.signed">uftt-linux.i386-deb-1.16.343</a>
+</p>
+<div style="display:none;">
+-----END RSA CHECKED CONTENT-----
+</div>
+<div style="display:none;">
+-----BEGIN RSA SIGNIATURE-----
+Q7UbGK1rDJSqyvBJCsosSZQ4GhAAvR+LBv89yTWv33ZXhBA01Ri7Vm4dL0sCO/WzT4Ka6kXRrKmkzkEUT4jc6s38sQ8CQNX6afv37FOgBK2ZhEoY4MorHJCj/uov16J1MDi5C41t+P/vUgzyKnrp46x7bK8Z8P1FLgVc/ua9KTfbPzX9NY0jIdiaYPzgULtKB6dzqdKvSt9rT3whSB5Pa1OW/icR+lSY9J/Q7gJhmq47ex5Ex68NNCe2TS6dK2vDG1MubD7pBjSgm+ItjakZPjjlr/eBinCO2on0uexIor+cgc73vIOMArrfXpMlv3wnUw2hmujBkuwlvFUKyxzMoBUl/JQHVGgpOgNpXzKvjP152vma4wJ3hTOVOc5k+37SiR3kNaizdDFqNq6Z+XG0oJFhQEnjf6+61Xcw0eK66Wv2jSrXwWKxOx5eISSmsm02NwNa6e6aSlajLvZ9CTWE4i1iucvNlTrBGqd+AtszblBXCR9Y11wO3fmWH/5U6CqY/NjgUNzF3pWNnHBTZCKCeGxmSLt+/9LXxyWpdcimDzpDBhkGb9LgpwO2qic8BU6yZiRSJj9EsjQ14fYt1sC2nw+TT8cVoE4d1yYsJCdA44yJWq7o2yy6boFS+tXjPMPAgg5KZl2qgPAa+A1wGX5jNn7bN9KHU9RJei+dJ5yIkQA=
+-----END RSA SIGNIATURE-----
+</div>
---
+++ site/bootstrap.php
@@ -0,0 +1,70 @@
+<?php
+
+require("../../php/sqlinfo.php");
+
+$reg = ($_REQUEST['reg'] == '1');
+$dbg = ($_REQUEST['dbg'] == '1');
+$port = (int)($_REQUEST['port']);
+$type = preg_replace("/[^a-zA-Z0-9]/", "", $_REQUEST['type']);
+$class = preg_replace("/[^a-zA-Z0-9]/", "", $_REQUEST['class']);
+if ($port === 0) $port = 47189;
+
+if ($reg) {
+	$query = 'INSERT INTO `hackykid_db`.`peers` (' .
+	'`id` ,' .
+	'`address` ,' .
+	'`port` ,' .
+	'`time` ,' .
+	'`type` ,' .
+	'`class`' .
+	')' .
+	'VALUES (' .
+	"NULL , '" . $_SERVER['REMOTE_ADDR'] . "', '" . $port ."', NOW( ), '" . $type . "', '" . $class . "'" .
+	');';
+	$result = mysql_query($query);
+}
+
+$query = 'SELECT NOW() as time';
+$result = mysql_fetch_array(mysql_query($query), MYSQL_ASSOC);
+$sqlnow = $result['time'];
+$phpnow = strtotime( $sqlnow );
+
+$sqlt4 = date( 'Y-m-d H:i:s', $phpnow - (4*60*60) );
+$sqlt1 = date( 'Y-m-d H:i:s', $phpnow - (1*60*60) );
+
+if (true) {
+	// delete old records
+	$query = "DELETE FROM peers WHERE (time < '" . $sqlt4 . "')";
+	$result = mysql_query($query);
+}
+
+if ($dbg)
+	//$query = 'SELECT * FROM peers';
+	$query = "SELECT * FROM peers ";
+else
+	$query = "SELECT DISTINCT address,port FROM peers ";
+$query .= "WHERE (time > '" . $sqlt1 . "') AND (type = '" . $type . "') AND (class = '" . $class . "')";
+$result = mysql_query($query);
+
+echo '*S*T*A*R*T*';
+echo chr(13);
+
+while($row = mysql_fetch_array($result, MYSQL_ASSOC))
+{
+	//echo expand($row);
+	if ($dbg) echo $row['id'] . chr(9);
+	echo $row['address'];
+	echo chr(9);
+	echo $row['port'];
+	if ($dbg) echo chr(9) . $row['time'];
+	if ($dbg) echo chr(9) . $row['type'];
+	if ($dbg) echo chr(9) . $row['class'];
+
+	echo chr(13);
+}
+
+echo '*S*T*O*P*';
+echo chr(13);
+
+mysql_close($conn);
+?>
