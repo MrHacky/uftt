3987c1a5f6889afa938716477418896f90d07a65
Simon Sasburg <simon.sasburg@gmail.com>
2008-11-30 13:58:08 +0000

    New autoupdate.php

--- site/autoupdate.php
+++ site/autoupdate.php
@@ -2,33 +2,14 @@
 -----BEGIN RSA CHECKED CONTENT-----
 </div>
 <p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r439.exe">uftt-win32.nt-static-1.24.439</a>
+<a href="http://uftt.googlecode.com/files/uftt-xp-r482.exe">uftt-win32.nt-static-1.26.482</a>
 <p>
-<a href="http://uftt.googlecode.com/files/uftt-9x-r439.exe">uftt-win32.9x-static-1.24.439</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r401.exe">uftt-win32.nt-static-1.22.401</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-9x-r401.exe">uftt-win32.9x-static-1.22.401</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r399.exe">uftt-win32.nt-static-1.20.399</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-9x-r399.exe">uftt-win32.9x-static-1.20.399</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r372.exe">uftt-win32.nt-static-1.18.372</a>
-<br>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r343.exe">uftt-win32.nt-static-1.16.343</a>
-<br>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r320.exe">uftt-win32.nt-static-1.12.320</a>
-<br>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r273.exe">uftt-win32.nt-static-1.10.273</a>
-<br>
-<a href="http://hackykid.heliohost.org/site/files/uftt-0.1.343-Linux.deb.signed">uftt-linux.i386-deb-1.16.343</a>
-</p>
+<a href="http://uftt.googlecode.com/files/uftt-9x-r482.exe">uftt-win32.9x-static-1.26.482</a>
 <div style="display:none;">
 -----END RSA CHECKED CONTENT-----
 </div>
 <div style="display:none;">
 -----BEGIN RSA SIGNIATURE-----
-WQWKWhd0vUaMxoeAXtl/TQbGDh6PkhF1jWI+7a6+0HVkimQT+KdQSwMR63Crq/Qc0l4pNr5ptfXrhvK+zumV8YBeXXCXnlwHglXv5agBMx5Hdu0H3TiVTb2fuZ06oxakfjTiGcB4kD614Tk4BDMjodbrVjK0qcdyGNM3CgbGvb+4oEEY0BTTfKX/jTr1M/phQTLnO7xMhdlwZ9PlMvrx8YW4dfNzGJCKU5Mg4di/URjhnu1PX4MFIjBAY/5NoKN0zyyliuPGXV+xstbyXIWxKyvjuX+74S1aNDx6gaMDZfW6Z9KLdsnHWshi5tSr+n+Z3LT1Zb/FYhbp4zpx3VwEwDRu2hSAnIrW0cEOpiqWYHj344PDaezRE3+tC6EM7O5g2UyeeoXQX0N6fjNT7PXZi/LM2akhoq/TmA4G5Yrful7EprUySnwzw4p6OoWopOcg4+D8aKI9ZGFcTrYIb1eLoJRoELffUVFa0svRJFoyLHgr+X2Lu2ESRSFbWCBkwSjSBBifTOO/VNUP3l2hoEUioyAj+sAhY8cvyr6iVtcYo1abcqq5NFP7ESBr/C2f2NTZ+2GxW5Kj3Qh+JfqksHZfYeFXGV9C73AxuWPpJqRAUmaRwH3+hCV+rGpTt0kmfkHTNVkG61LVanSj/RJ+8a87lrwKHlQl0aewuCnqqKMBwT0=
+Si+O4il4rx1fvgOiO5jbgX/TdNbq2nSS7BeZVaoRHZ81VuRYMVJLBqyxzGLzl72Lz+JpQFwjr3vRLWaOU9O1x9gzmE3ip3ajdeQ3dup47d6mMvkOMtV5AOVuFBpYWk90+IVaoPJI17eEolOhyDnSz4MdbyknLL+Ygix/blzE3nBIMDAFa5CCpuJ/+qYkPkgXKElB3LY2NpoKC0+MfLdiXgJNTZYJ4/N/nUF7PRvEeZ0wJHIh1GWpMMOOMbySYglnt7cKOkqwrvsySyUeubCu/t34JbckHt+9vTxOlUdjI4PVynUlMgZUtlV+iq0QUEH7N3T8L4ACzrRq+8wLWMCzCb7ZyuJNu9QNsTYiMftFFfi5sImF2Fv9uwzJUdBl8fWuswlSVXuzph/Htktzi4K2YRmSAKqGtBfee8kzYqW8d7SEArdPuMPJ8Ys/nU/i5I3ndDXNbtDCNZ2MOUC/LKrKpa75OePgVbML9gabCNHk/rR8wKkP7uLr8WL5r5hBuNuYQ2frDZwCe+6dhjyHXb8JfXy8pRuP7voHVqa7jU9iREm/4QmzK5jMaM8LUE4r1W75XQw7EE02+Zpbx4hVz1qogOEQY4vVm6W/VPF1d9U5cbmt8ZksISUaWyJ3DPE7pvj/cb9KJOO1wjIwQkCrNFk1u/wpr0UtBn3/GbEdHuViBBQ=
 -----END RSA SIGNIATURE-----
 </div>
