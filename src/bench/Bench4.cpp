989c7df150041b2a5be8a5d12f3399844bfd7f91
Simon Sasburg <simon.sasburg@gmail.com>
2008-12-14 13:04:52 +0000

    Make bench4 compile under linux again.

--- src/bench/Bench4.cpp
+++ src/bench/Bench4.cpp
@@ -35,7 +35,7 @@ int APIENTRY synesisWinMain(HINSTANCE,
 #  include <linux/if_addr.h>
 #  include <linux/if_link.h>
 extern "C" {
-#  include "../bench/libnetlink.h"
+#  include "../linux/libnetlink.h"
 }
 #  include <net/if.h>
 #  include <net/if_arp.h>
