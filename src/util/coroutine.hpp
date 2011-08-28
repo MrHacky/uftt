f1b396d5fb00daa4072f89e842ab8d3a18bee55b
Simon Sasburg <simon.sasburg@gmail.com>
2011-08-28 21:48:27 +0000

    Be a bit more friendly to our optimizing compilers.
    Use __COUNTER__ instead of __LINE__ when possible, and avoid
    negative switch cases, so the generated switch statement can
    be translated more easily into smaller jumptables.

--- src/util/coroutine.hpp
+++ src/util/coroutine.hpp
@@ -15,9 +15,9 @@ class coroutine
 {
 public:
   coroutine() : value_(0) {}
-  bool is_child() const { return value_ < 0; }
-  bool is_parent() const { return !is_child(); }
-  bool is_complete() const { return value_ == -1; }
+  bool is_parent() const { return value_ < 0; }
+  bool is_child() const { return !is_parent(); }
+  bool is_complete() const { return value_ == 1; }
 private:
   friend class coroutine_ref;
   int value_;
@@ -28,7 +28,7 @@ class coroutine_ref
 public:
   coroutine_ref(coroutine& c) : value_(c.value_), modified_(false) {}
   coroutine_ref(coroutine* c) : value_(c->value_), modified_(false) {}
-  ~coroutine_ref() { if (!modified_) value_ = -1; }
+  ~coroutine_ref() { if (!modified_) value_ = 1; }
   operator int() const { return value_; }
   int& operator=(int v) { modified_ = true; return value_ = v; }
 private:
@@ -39,11 +39,11 @@ private:
 
 #define CORO_REENTER(c) \
   switch (coroutine_ref _coro_value = c) \
-    case -1: if (_coro_value) \
+    case 1: if (_coro_value) \
     { \
       goto terminate_coroutine; \
       terminate_coroutine: \
-      _coro_value = -1; \
+      _coro_value = 1; \
       goto bail_out_of_coroutine; \
       bail_out_of_coroutine: \
       break; \
@@ -68,20 +68,20 @@ private:
             else case 0:
 
 #define CORO_FORK_IMPL(n) \
-  for (_coro_value = -(n);; _coro_value = (n)) \
-    if (_coro_value == (n)) \
+  for (_coro_value = (n);; _coro_value = -1) \
+    if (_coro_value == -1) \
     { \
-      case -(n): ; \
+      case (n): ; \
       break; \
     } \
     else
 
-#if defined(_MSC_VER)
-# define CORO_YIELD CORO_YIELD_IMPL(__COUNTER__ + 1)
-# define CORO_FORK CORO_FORK_IMPL(__COUNTER__ + 1)
-#else // defined(_MSC_VER)
-# define CORO_YIELD CORO_YIELD_IMPL(__LINE__)
-# define CORO_FORK CORO_FORK_IMPL(__LINE__)
-#endif // defined(_MSC_VER)
+#if defined(__COUNTER__)
+# define CORO_YIELD CORO_YIELD_IMPL(__COUNTER__ + 2)
+# define CORO_FORK CORO_FORK_IMPL(__COUNTER__ + 2)
+#else // defined(__COUNTER__)
+# define CORO_YIELD CORO_YIELD_IMPL(__LINE__ + 1)
+# define CORO_FORK CORO_FORK_IMPL(__LINE__ + 1)
+#endif // defined(__COUNTER__)
 
 #endif // COROUTINE_HPP
