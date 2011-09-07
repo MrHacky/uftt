94abde76199d127d967d5e9a6cfe6ba934cac465
Simon Sasburg <simon.sasburg@gmail.com>
2011-09-07 13:17:37 +0000

    Second try, new release in web update.

--- site/autoupdate.php
+++ site/autoupdate.php
@@ -2,15 +2,11 @@
 -----BEGIN RSA CHECKED CONTENT-----
 </div>
 <p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r610.exe">uftt-win32.nt-static-1.30.610</a>
+<a href="http://uftt.googlecode.com/files/uftt-r959.exe">uftt-win32.nt-static-2.959.2011_9_7_15_07_53.1500</a>
 <p>
-<a href="http://uftt.googlecode.com/files/uftt-9x-r610.exe">uftt-win32.9x-static-1.30.610</a>
+<a href="http://uftt.googlecode.com/files/uftt-r959-win9x.exe">uftt-win32.9x-static-2.959.2011_9_7_14_57_55.1500</a>
 <p>
 <a href="http://uftt.googlecode.com/svn/files/uftt-0.1.610-Linux.deb.signed">uftt-linux.i386-deb-1.30.610</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-xp-r482.exe">uftt-win32.nt-static-1.26.482</a>
-<p>
-<a href="http://uftt.googlecode.com/files/uftt-9x-r482.exe">uftt-win32.9x-static-1.26.482</a>
 <p>
 <a href="http://uftt.googlecode.com/svn/files/uftt-0.1.343-Linux.deb.signed">uftt-linux.i386-deb-1.16.343</a>
 <div style="display:none;">
@@ -18,6 +14,21 @@
 </div>
 <div style="display:none;">
 -----BEGIN RSA SIGNIATURE-----
-H7nukD8zIIM+Gw66LwtWMafSlEHOFWFdLlPZrFdvrnSPR4g9QhrZW1Y13YnmGM+BBFT7RQ3f6g/yD+/eA481RcvaNSLA49evxqOuPNr6M1DPBNkWqLxsJX/vEMARJN35zwzJ+DkM8s4DqfasClp+2Ewdp4w3Mz+UAABYKPEktX+kf+v1l2GDerViNF5YPj843A+qpO/SCW0EelQMFdOizOZ5Y/QA0P/wueV2R9NoncjD63k4lC4bHm1o+hKwy/9JGBts207k7Rg2NnoIjCWHw7iVEG/MH08C9z6fw3hhugYsijwjJCptW1s2CypPEQ2Ep5ZwkdRhG5Ka6HRwgvrnD1aBFgl9o+6/pRfsnDo9yel2N14McOzdbYLKDJ2lIHcHnUCUo1GwuiH5aO5TZje6SBYBVlsFXTMh7B/AkF0BhKgnI5IQfQ3tuf0hwJcxXzDuJrT13E/lrTKWBFaO0YLjPsxScgXuiDhJuKGYS2UnIyJsXsjCyRI6A/z9oi2safZIqZwCYQ//jAIBgimbU+Oue1ucfjpFeEBiqextkGJft3yWy5JYMOKmMxAyVLfZbMF3vMTkDj/LevgblIi/axEVW4hv8rWRTzBa1m/pRuvMQxoSf3tFI1BDBr4uDQXFgy8NXQk3DXWpqZLkqfAgSdf3/eu0KUDUDaVA/aRFX2eaizg=
+rf/VToaWTk2Lvi8Vt9n5bwrod+bwq9LbdjcJ5ivcuJAO89hTiAQ0LLIXVkxnCNuNFgr6JsEY9piscTR5sz+2qd2ZRo4eJTu8SU2Vyp1JdSY3VykZPPXY8pOk8s1MiaylVwoOnItRLZAPPG2ZpCJEm9mFc8XTom1CpS8A7scp9qn4xxuLlD94vDvTELwGs6T9RlREHUI+AOjC02ybJRx81EEYmfhn+++R6ir5iMTTXFdbceyC8BLMkRKiFU8sz2mvgbTOspS3WuJ95wLLJuV/pYyFMVxwLlNMJR6cdWpmCspHaCVv8epLrpZulZs4u5qDvOHWrfd7ZN36TaD4G6oVRr2E+i9xu+IgX0BWPhOb/+xBp649JClhSHkbaVxhPpNFqI8e6eew2yOcC2lCeY7OA2SLiLyfftYF//OhZZ4io1ixcNNf6JuyRqN4hYoRMXToLWDaUeA4UrG+4SkSUnNDHyaYUeDYfaXJ1TEJvFUqLzPY2RH7XXWu5iqaxWeVWkGtm/W5Jy3IStUdrd4iBcMMaRI0gimsY6s6VGRKH/dCF+kbQAbbe5idwxiI0YnGBC3W1G7PfNKJyLtFDqhxXGyEmTuo5zU78rsZ2q9jvn+oETL7k2xD816YC7nKs+O4aq40ju8idAKB8pEdeqDmkZPUe58iukjcdZizNGADSa0ovHs=
+-----END RSA SIGNIATURE-----
+</div>
+<div style="display:none;">
+-----BEGIN RSA CHECKED CONTENT-----
+</div>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-r959.exe">uftt-win32.nt-static-2.959.2011_9_7_15_07_53.1500</a>
+<p>
+<a href="http://uftt.googlecode.com/files/uftt-r959-win9x.exe">uftt-win32.9x-static-2.959.2011_9_7_14_57_55.1500</a>
+<div style="display:none;">
+-----END RSA CHECKED CONTENT-----
+</div>
+<div style="display:none;">
+-----BEGIN RSA SIGNIATURE-----
+lW52zOn7Bpk4Z5J9clVzdi7FpfBprPsaEYDSVWTZYCzYlx10GcU4xsC8lDK6fno31x+vhCXAURLo2tUDFMg9g1Pj4XlcvidmwaD+HX/4t1JaRDtDuee95EWqCI72dvDmCD+qgRv0UXxKUYUVmKtWCLP2dwlZp+RxZdhvKNogWLXVkaoVaURbQtCYcJm7/+zIRS79HKa1PuG7KjvKwiI5hOXAH4Oby0xNxWjgFG+r8QLUDAc23uUGn4qmPtFQp82J10AfBqBep3BjccuijgjoCmuANdhkHvxxgLWxHZUREyGveJ4eWUagJP+Wmyw9MRLYF1QbDAny+lzKY3k1HWCEGd7aoNeJSsv6n1fuHHsP35CQxSx/3LcF14ialBO7jVmb7TYL5BSs/Dux5nEkvN7wA3hV5cWvisA2/K2K6GNXOamUSsNxo99TenAmtOe58GO/XRZGc+IYExPp25rP6S7suXymq9Eeo6I2SkMIXLWZBSLL8MScBpm2Yp7NzudJ158TryP0P+tm8AjD3ejUDpimpltObfkTFOtVtphLKrguNLLaIzPRLqhZE5D2fBouJGa3q/9RYCeSZhdchngwIBtVtUMxLRfYuyrJywZH8DPj5HXR+nBCqTJLkA9LLPNz74ZJsJiXr0J9bk8RUazCwKRaDlEHV/GYLSMLE9vymXeVre8=
 -----END RSA SIGNIATURE-----
 </div>
