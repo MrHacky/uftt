a042c40f43dcb9be2b113735e7810895e4960bc7
Simon Sasburg <simon.sasburg@gmail.com>
2011-10-23 18:26:10 +0000

    Fix some cmake scripts, use policy for empty items in lists.

--- cmake/Helpers/svnrev-git.cmake
+++ cmake/Helpers/svnrev-git.cmake
@@ -1,7 +1,9 @@
+cmake_policy(SET CMP0007 NEW)
+
 File(STRINGS "${INPUT}" LINES REGEX "^r[0-9]*$")
 List(REVERSE LINES)
 List(GET LINES 0 LINE)
-String(SUBSTRING "${LINE}" 1 -1 REV)
+String(REGEX REPLACE "^r" "" REV "${LINE}")
 
 File(WRITE  "${OUTPUT}" "// generated file\n")
 File(APPEND "${OUTPUT}" "#define SVN_REVISION_STRING \"${REV}\"\n")
--- cmake/Helpers/svnrev-svn.cmake
+++ cmake/Helpers/svnrev-svn.cmake
@@ -1,5 +1,7 @@
+cmake_policy(SET CMP0007 NEW)
+
 File(STRINGS "${INPUT}" LINES)
-List(GET LINES 2 REV)
+List(GET LINES 3 REV)
 
 File(WRITE  "${OUTPUT}" "// generated file\n")
 File(APPEND "${OUTPUT}" "#define SVN_REVISION_STRING \"${REV}\"\n")
