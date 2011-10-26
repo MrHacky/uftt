81adec3d2f0850975bc794b6db2cf56b2df55e9b
Simon Sasburg <simon.sasburg@gmail.com>
2011-10-26 02:18:14 +0000

    Implement UFTTCore::GUI_CMD_QUIT for cuftt.
    This makes it pass the quit1 test.

--- src/console-ui/cuftt.cpp
+++ src/console-ui/cuftt.cpp
@@ -8,10 +8,22 @@
 
 #include <iostream>
 
+struct servicelocker {
+	boost::mutex mutex;
+	boost::asio::io_service* service;
+};
+
 class ConsoleMain : public UFTTGui {
 	public:
+		boost::asio::io_service service;
+		boost::shared_ptr<boost::asio::io_service::work> work;
+		boost::shared_ptr<servicelocker> lockedservice;
+
 		ConsoleMain(int argc, char **argv, UFTTSettingsRef settings)
+		: work(new boost::asio::io_service::work(service))
+		, lockedservice(new servicelocker())
 		{
+			lockedservice->service = &service;
 		}
 
 		~ConsoleMain()
@@ -20,14 +32,41 @@ class ConsoleMain : public UFTTGui {
 
 		void bindEvents(UFTTCore* core)
 		{
+			core->connectSigGuiCommand(boost::bind(&ConsoleMain::handleGuiCommand, this, _1));
+		}
+
+		void handleGuiCommand(UFTTCore::GuiCommand cmd)
+		{
+			if (cmd == UFTTCore::GUI_CMD_QUIT) {
+				work.reset();
+			}
+		}
+
+		void handleInput(const std::string& line)
+		{
+			work.reset();
 		}
 
 		int run()
 		{
-			std::cout << "Press enter to exit\n";
-			std::string s;
-			std::cin >> std::noskipws >> s;
+			boost::thread inputthread(boost::bind(&ConsoleMain::input, this, lockedservice));
+			service.run();
+			boost::mutex::scoped_lock lock(lockedservice->mutex);
+			lockedservice->service = NULL;
 			return 0;
+		}
+
+		static void input(ConsoleMain* cmain, boost::shared_ptr<servicelocker> lockedservice)
+		{
+			std::cout << "Press enter to exit\n";
+			std::string line;
+			while (std::getline(std::cin, line)) {
+				boost::mutex::scoped_lock lock(lockedservice->mutex);
+				if (lockedservice->service)
+					lockedservice->service->post(boost::bind(&ConsoleMain::handleInput, cmain, line));
+				else
+					return;
+			}
 		}
 };
 
