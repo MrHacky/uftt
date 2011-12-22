712779ce589af34d376fd60b55bf78429995ded4
Daniël Geelen <daniel.geelen+github@gmail.com>
2011-12-22 15:09:24 +0000

    [tests] look harder for a netcat.

--- tests/testdriver.sh
+++ tests/testdriver.sh
@@ -2,6 +2,29 @@
 
 trap handle_exit ERR
 
+COLOR_WARN="\033[01;33m"
+COLOR_ERROR="\033[01;31m"
+COLOR_STATUS="\033[01;32m"
+COLOR_HIGHLIGHT="\033[01;36m"
+COLOR_DEBUG="\033[01;35m"
+COLOR_DEFAULT="\033[00m"
+function message() {
+	COLOR_CODE=$(eval echo "\${${1}}")
+	echo -e "> ${COLOR_CODE}${2}${COLOR_DEFAULT}" 1>&2
+}
+
+function die() {
+	message COLOR_ERROR "${1}"
+	exit 1
+}
+
+# Find a netcat binary. The original netcat uses 'nc', whereas gnu-netcat
+# uses 'ncat' and 'netcat'.
+NETCAT="$(which nc ncat netcat 2>/dev/null | head -1)"
+if [ -z "${NETCAT}" ] ; then
+	die "Error: could not locate a 'netcat' binary. Please install gnu-netcat or similar, then try again."
+fi
+
 # strict mode, all commands must have 0 exit code, no unfinished jobs
 function handle_exit {
 	local EXITCODE=$?
@@ -30,10 +53,10 @@ function handle_exit {
 # Start an uftt instance in the background, and wait for it to spin up
 # UFTTPID is set to the pid of the started instance
 function start_uftt_bg {
-	if (nc -h 2>&1 | grep -o -i bsd); then
-		nc -l 47187&
+	if ("${NETCAT}" -h 2>&1 | grep -o -i bsd); then
+		"${NETCAT}" -l 47187&
 	else
-		nc -l -p 47187&
+		"${NETCAT}" -l -p 47187&
 	fi
 	NCPID=$!
 
