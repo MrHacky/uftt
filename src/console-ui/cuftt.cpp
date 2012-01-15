eba98f8011725e0a551b25a0cd36aaeb44edd90c
Simon Sasburg <simon.sasburg@gmail.com>
2012-01-15 12:35:47 +0000

    Add some simple functions to console ui.
    You can now add local shares with 's <path>'.
    You can list local shares with 'l', which lists shares with an identifier at
    the start of the line. That identifier can be used with 'd <id> <path>' to download
    the share.

--- src/console-ui/cuftt.cpp
+++ src/console-ui/cuftt.cpp
@@ -6,7 +6,10 @@
 #include "../UFTTGui.h"
 #include "../UFTTCore.h"
 
+#include "../util/StrFormat.h"
+
 #include <iostream>
+#include <set>
 
 struct servicelocker {
 	boost::mutex mutex;
@@ -19,6 +22,10 @@ class ConsoleMain : public UFTTGui {
 		boost::shared_ptr<boost::asio::io_service::work> work;
 		boost::shared_ptr<servicelocker> lockedservice;
 
+		UFTTCore* core;
+		std::map<std::string, ShareID> sharemap;
+		std::set<ShareID> shareset;
+
 		ConsoleMain(int argc, char **argv, UFTTSettingsRef settings)
 		: work(new boost::asio::io_service::work(service))
 		, lockedservice(new servicelocker())
@@ -30,9 +37,11 @@ class ConsoleMain : public UFTTGui {
 		{
 		}
 
-		void bindEvents(UFTTCore* core)
+		void bindEvents(UFTTCore* core_)
 		{
+			core = core_;
 			core->connectSigGuiCommand(boost::bind(&ConsoleMain::handleGuiCommand, this, _1));
+			core->connectSigAddShare(service.wrap(boost::bind(&ConsoleMain::addShare, this, _1)));
 		}
 
 		void handleGuiCommand(UFTTCore::GuiCommand cmd)
@@ -42,13 +51,43 @@ class ConsoleMain : public UFTTGui {
 			}
 		}
 
+		void addShare(const ShareInfo& info)
+		{
+			if (shareset.count(info.id)) return;
+			std::string key = STRFORMAT("%d", shareset.size());
+			if (info.isupdate) return;
+			std::cout << key << ": " << info.name << "\t(" << info.host << ", " << info.user << ")\n";
+			sharemap[key] = info.id;
+			shareset.insert(info.id);
+		}
+
 		void handleInput(const std::string& line)
 		{
-			work.reset();
+			std::vector<std::string> items;
+			boost::split(items, line, boost::is_any_of(" \t"));
+			if (line == "" || items.empty() || items[0] == "q" || items[0] == "quit")
+				work.reset();
+			else if (items[0] == "l" || items[0] == "list")
+				refresh();
+			else if ((items[0] == "d" || items[0] == "download") && items.size() == 3)
+				core->startDownload(sharemap[items[1]], items[2]);
+			else if ((items[0] == "s" || items[0] == "share") && items.size() == 2)
+				core->addLocalShare(items[1]);
+			else
+				std::cout << "[q]uit, [l]ist, [d]ownload <id> <path>, [s]hare <path>, [r]emove <id> [h]elp\n";
+		}
+
+		void refresh()
+		{
+			std::cout << "Refresh!\n";
+			sharemap.clear();
+			shareset.clear();
+			core->doRefreshShares();
 		}
 
 		int run()
 		{
+			std::cout << "Press enter to exit\n";
 			boost::thread inputthread(boost::bind(&ConsoleMain::input, this, lockedservice));
 			service.run();
 			boost::mutex::scoped_lock lock(lockedservice->mutex);
@@ -58,7 +97,6 @@ class ConsoleMain : public UFTTGui {
 
 		static void input(ConsoleMain* cmain, boost::shared_ptr<servicelocker> lockedservice)
 		{
-			std::cout << "Press enter to exit\n";
 			std::string line;
 			while (std::getline(std::cin, line)) {
 				boost::mutex::scoped_lock lock(lockedservice->mutex);
