393df209b68875f5c5a10507033a5793c2111a03
Simon Sasburg <simon.sasburg@gmail.com>
2012-12-09 13:26:40 +0000

    Fix error when using boost 1.52

--- src/UFTTCore.cpp
+++ src/UFTTCore.cpp
@@ -350,7 +350,7 @@ void UFTTCore::initialize()
 	for (uint i = 0; i < netmodules.size(); ++i)
 		netmodules[i]->setModuleID(i);
 
-	servicerunner = boost::thread(boost::bind(&UFTTCore::servicerunfunc, this)).move();
+	servicerunner = boost::move(boost::thread(boost::bind(&UFTTCore::servicerunfunc, this)));
 
 	ext::filesystem::path apppath = platform::getApplicationPath();
 	if (!apppath.empty())
